e9ab5c63fd4320d519078673a9c01b457a3c02bd
Logan Sage <lvdsage@gmail.com>
2021-02-24 21:17:49 -0700

    created mongoose exercise schema.

---
+++ models/workouts.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const schema = mongoose.schema;
+const exerciseSchema = new schema({
+    day:{
+        type:Date,
+        default:Date.now,
+    },
+    exercise:[{
+        type:{
+            type:String,
+            trim:true,
+            required:"THE TYPE OF EXERCISE IS REQUIRED"
+        },
+        name:{
+            type:String,
+            trim:true,
+            required:"THE EXERCISE NAME IS REQUIRED"
+        },
+        weight:{
+            type:Number,
+        },
+        sets:{
+            type:Number,
+        },
+        reps:{
+            type:Number,
+        },
+        duration:{
+            type:Number,
+            required:"THE EXERCISE DURATION IS REQUIRED"
+        },
+        distance:{
+            type:Number, 
+        }
+    }]
+})
+const exercise = mongoose.model("exercise",exerciseSchema);
+module.exports = exercise;
